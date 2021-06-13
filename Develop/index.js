a90998b6e7a180fb3af2fad949e6195c0d43af11
Melissa Santos <melissasantos@melissas-mbp.attlocal.net>
2021-06-13 19:40:55 -0400

    added packages needed and started array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+const { type } = require("os");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "Title",
+    message: "What is the title of the project?",
+
+}, {
+    type: "input",
+    name: "Description",
+    message: "Give a desciption about your project."
+
+}, {
+    
+}
+   
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
