bc746a2f75075a565aed98be161e7ada238aa61f
Melissa Santos <melissasantos@melissas-mbp.attlocal.net>
2021-06-13 21:28:38 -0400

    function to write readme file

--- Develop/index.js
+++ Develop/index.js
@@ -60,13 +60,23 @@ const questions = [{
     name: "Email",
     message: "What is your email address?"
 
-}
-   
-
+},
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log (err);
+        }
+        else {
+            console.log("Success");
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
